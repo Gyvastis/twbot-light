5e53a23521fd2a6dba50b083ac84ddb85c55e0d6
Vaidas <shibalba1@gmail.com>
2016-10-14 19:33:24 +0300

    add media dir

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 
 define('BASE_DIR', __DIR__ . '/');
 define('DATA_DIR', BASE_DIR . 'data/');
+define('MEDIA_DIR', BASE_DIR . 'media/');
 define('ROUTE_DIR', BASE_DIR . 'routes/');
 
 define('ACCOUNTS_DATA_FILE', DATA_DIR . 'accounts.yml');
