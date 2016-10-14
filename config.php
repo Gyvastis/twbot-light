d4710c757705400088fcd5c3b54289d5caac945d
Vaidas <shibalba1@gmail.com>
2016-10-14 19:58:24 +0300

    add logs folder

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 
 define('BASE_DIR', __DIR__ . '/');
 define('DATA_DIR', BASE_DIR . 'data/');
+define('LOG_DIR', BASE_DIR . 'logs/');
 define('MEDIA_DIR', BASE_DIR . 'media/');
 define('ROUTE_DIR', BASE_DIR . 'routes/');
 
