919fc124efa254072245db0f966c1c685a4cd9e1
Vaidas <shibalba1@gmail.com>
2016-10-15 10:00:36 +0300

    getProvider function form pimple

--- container.php
+++ container.php
@@ -6,6 +6,17 @@ $container = new \Slim\Container([
     'displayErrorDetails' => true,
 ]);
 
-$container['errorHandler'] = function ($c) {
+/**
+ * @param $serviceName
+ * @return object
+ */
+function getProvider($serviceName){
+    global $container;
+    $serviceName = lcfirst($serviceName);
+
+    return $container[$serviceName];
+}
+
+$container['errorHandler'] = function ($container) {
     return \Twbot\Factory\LoggerFactory::getDefaultErrorHandler();
 };
--- routes/post.php
+++ routes/post.php
@@ -17,8 +17,9 @@ $app->get('/ping-proxy/{id}', function ($request, $response, $args) {
     $id = $request->getAttribute('id');
     //check param excaption
 
-    $proxy = \Twbot\Repository\ProxyRepository::getProxyById($id);
     $logger = \Twbot\Factory\LoggerFactory::getLogger(\Twbot\Enumerator\LoggerEnumerator::PROXY_LOGGER);
+
+    $proxy = \Twbot\Repository\ProxyRepository::getProxyById($id);
     $proxyService = new \Twbot\Service\ProxyService($proxy, $logger);
     $result = $proxyService->ping() ? 'Worked!' : 'Nayyy..';
 
@@ -29,18 +30,15 @@ $app->get('/{username}', function ($request, $response, $args) {
     $username = $request->getAttribute('username');
     //check param excaption
 
+    $logger = \Twbot\Factory\LoggerFactory::getLogger(\Twbot\Enumerator\LoggerEnumerator::POST_LOGGER);
     $account = \Twbot\Repository\AccountRepository::getAccountByUsername($username);
+    $twitter = \Twbot\Factory\TwitterFactory::getTwitterOAuth($account);
+    $image = \Twbot\Factory\ImageFactory::getRandomImage($account);
 
     $message = new \Twbot\Entity\Message();
     $message->setMessage('Test first message');
 
-    $postService = new \Twbot\Service\PostService(
-        \Twbot\Factory\TwitterFactory::getTwitterOAuth($account),
-        $account,
-        $message,
-        \Twbot\Factory\ImageFactory::getRandomImage($account),
-        \Twbot\Factory\LoggerFactory::getLogger(\Twbot\Enumerator\LoggerEnumerator::POST_LOGGER)
-    );
+    $postService = new \Twbot\Service\PostService($twitter, $account, $message, $image, $logger);
     $postService->send();
 
     return $response->write("Posted to: $username");
