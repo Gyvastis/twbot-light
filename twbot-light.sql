6a713026e6d612f94fcb1a05aabdc355553944b7
Vaidas Bagdonas <shibalba1@gmail.com>
2017-01-25 22:21:02 +0100

    update sql dump to latest structure

--- twbot-light.sql
+++ twbot-light.sql
@@ -1,41 +1,22 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.46-0ubuntu0.14.04.2)
-# Database: scotchbox
-# Generation Time: 2016-10-18 15:53:55 +0000
-# ************************************************************
+-- Adminer 4.2.5 MySQL dump
 
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table cron_job
-# ------------------------------------------------------------
-
+DROP TABLE IF EXISTS `cron_job`;
 CREATE TABLE `cron_job` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL DEFAULT '',
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `is_last` tinyint(4) NOT NULL,
+  `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
-# Dump of table followers_free
-# ------------------------------------------------------------
-
+DROP TABLE IF EXISTS `followers_free`;
 CREATE TABLE `followers_free` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `seeder_username` varchar(255) NOT NULL,
@@ -58,10 +39,7 @@ CREATE TABLE `followers_free` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
-# Dump of table followers_used
-# ------------------------------------------------------------
-
+DROP TABLE IF EXISTS `followers_used`;
 CREATE TABLE `followers_used` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL DEFAULT '',
@@ -70,11 +48,4 @@ CREATE TABLE `followers_used` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2017-01-25 21:20:02
